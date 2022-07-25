1528f3cc0eed865dc7c5622e7a2bccf684efee7d
ejade2816 <71690723+ejade2816@users.noreply.github.com>
2022-07-24 23:11:27 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          main9
+          main10
         </h1>
       </main>
     </div>
